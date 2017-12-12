76aaecb999bbded93214accee4e63ba931a739a1
theapphideaway <32747586+theapphideaway@users.noreply.github.com>
2017-12-12 08:53:51 -0500

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by iansc on 5/6/2017.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue ){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+
+    }
+}
